db1f31d8c1859a92a6cad6dca5e4a0f6f92d538e
rio004 <riyon336@gmail.com>
2025-09-17 20:00:10 +0100

    udfpshandler: Silence unused parameter warnings
    Change-Id: I47d1bee43357139971931b46cda486bc238e1f7a
    Signed-off-by: rio004 <riyon336@gmail.com>

--- fingerprint/include/UdfpsHandler.h
+++ fingerprint/include/UdfpsHandler.h
@@ -17,12 +17,12 @@ class UdfpsHandler {
   public:
     virtual ~UdfpsHandler() = default;
 
-    virtual void init(fingerprint_device_t* device) {};
-    virtual void onFingerDown(uint32_t x, uint32_t y, float minor, float major) {};
+    virtual void init(fingerprint_device_t* /*device*/) {};
+    virtual void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {};
     virtual void onFingerUp() {};
     virtual void onUiReady() {};
 
-    virtual void onAcquired(int32_t result, int32_t vendorCode) {};
+    virtual void onAcquired(int32_t /*result*/, int32_t /*vendorCode*/) {};
     virtual void onAuthenticationSucceeded() {};
     virtual void onAuthenticationFailed() {};
     virtual void cancel() {};
